025ab020da0a17f21efd7b9165db6a8f6df2a95d
mikaellq <38102084+mikaellq@users.noreply.github.com>
2018-05-18 14:52:51 +0200

    Initial commit

---
+++ GolfGame/GolfRound.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+static class Const
+{
+    public const double GRAVITY = 9.82432;
+    public const int INGAME = 0;
+    public const int FAIL = 1;
+    public const int HIT = 2;
+    public const int TOOMANY = 5;
+    public const double MARGIN = 0.5;
+}
+
+namespace GolfGame
+{
+    internal class GolfRound
+    {
+        int counter;
+        double pos = 0, hole = 0;
+        public GolfRound()
+        {
+            pos = 0; counter = 0;
+            Random r = new Random(DateTime.Now.Millisecond);
+            hole = r.Next(10, 30);
+            Console.WriteLine($"Position hole: {hole}, your: {pos}");
+        }
+
+        public int Shoot(double angle, double velocity)
+        {
+            double rangle = (Math.PI / 180) * angle;
+            double newputt = Math.Pow(velocity, 2) / Const.GRAVITY * Math.Sin(2 * rangle);
+            counter++;
+            if (pos > hole)
+            {
+                pos -= newputt;
+            }
+            else pos += newputt;
+
+            Console.WriteLine($"{counter}. New position: {pos}");
+
+            if (pos > hole*2 || pos < 0)
+            {
+                Console.WriteLine("EXCEPTIONALLY BADLY THROWN!");
+                return Const.FAIL;
+            }
+            else if (counter > Const.TOOMANY)
+            {
+                Console.WriteLine("EXCEPTIONALLY MANY THROWN!");
+                return Const.FAIL;
+            }
+            else if (pos < hole+Const.MARGIN && pos > hole-Const.MARGIN)
+            {
+                Console.WriteLine($"Congratulations! You hit the hole in {counter} tries!\n");
+                return Const.HIT;
+            }
+            return Const.INGAME;
+        }
+    }
+}
---
+++ GolfGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolfGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string angletxt, velocitytxt;
+            double angle, velocity;
+
+            do
+            {
+                int test = 0;
+                GolfRound round = new GolfRound();
+
+                do
+                {
+                    do
+                    {
+                        Console.Write("Angle: ");
+                        angletxt = Console.ReadLine();
+                    } while (!Double.TryParse(angletxt, out angle));
+
+                    do
+                    {
+                        Console.Write("Velocity: ");
+                        velocitytxt = Console.ReadLine();
+                    } while (!Double.TryParse(velocitytxt, out velocity));
+
+                    test = round.Shoot(angle, velocity);
+                    if (test == Const.FAIL)
+                    {
+                        try
+                        {
+                            throw new System.InvalidOperationException("You got too far away from the hole!");
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine("Ending the game!");
+                            //                        System.Console.WriteLine(ex);
+                        }
+                    }
+                } while (test == Const.INGAME);
+                Console.WriteLine("Press Enter to continue, any other key to exit:");
+            } while (Console.ReadKey().KeyChar == '\r');
+        }
+    }
+}
